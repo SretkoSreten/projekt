86b72eaa5248452faeb9147584c4d5f05b51f2c7
SretkoSreten <sretenperic3000@gmail.com>
2024-07-30 00:37:38 +0200

    last-commit

--- server/api/threads.js
+++ server/api/threads.js
@@ -1,27 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const { ThreadsAPI } = require('threads-api');
+const { ThreadsAPI } = require("threads-api");
 require("dotenv").config();
 
 router.post("/", async (req, res) => {
-  const { title, content } = req.body;
+
+  const { title, text } = req.body;
+  const image = req.file;
+
+  if (!image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "No image file provided" });
+  }
 
   try {
+
+    const fs = require('fs');
+
+    const imageBuffer = fs.readFileSync(image.path);
+
     const threadsApi = new ThreadsAPI({
       username: process.env.INSTAGRAM_USERNAME,
       password: process.env.INSTAGRAM_PASSWORD,
-    }); 
-
-    const response = await threadsApi.publish({
-      title,
-      content,
-      url: "https://bitmidi.com", // Adjust according to the parameters required
     });
 
+    const publishOptions = {
+      text,
+      attachment: {
+        sidecar: [
+          {
+            type: image.mimetype,
+            data: imageBuffer,
+          },
+          {
+            type: image.mimetype,
+            data: imageBuffer,
+          },
+        ],
+      },
+    };
+
+    const response = await threadsApi.publish(publishOptions);
+
     return res.status(200).json({ success: true, response });
+    
   } catch (error) {
-    return res.status(500).send("Error posting to Instagram Threads");
-  } 
+    console.error("Error posting to Instagram Threads:", error);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 });
 
 module.exports = router;
